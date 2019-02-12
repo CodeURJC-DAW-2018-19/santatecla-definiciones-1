016b9c33465eb2b23b48e98df7cec8134f56306a
ram2701 <rbarrabesp@gmail.com>
2019-02-12 10:00:44 +0100

    1 error solved in MyErrorController

--- santatecla-definiciones/src/main/java/com/grupo5/definiciones/MyErrorController.java
+++ santatecla-definiciones/src/main/java/com/grupo5/definiciones/MyErrorController.java
@@ -10,12 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyErrorController implements ErrorController  {
- 
-    @RequestMapping("/error")
-    public String runError() {
-        //do something like logging
-        return "error";
-    }
     
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
