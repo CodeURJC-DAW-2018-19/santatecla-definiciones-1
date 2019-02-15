645ad47f0f33ee6d747e6d47503225ead0473f46
LordHansTopo <32304288+LordHansTopo@users.noreply.github.com>
2019-02-15 02:50:01 +0100

    Add show more button
    Button is added but does not properly show more chapters

--- santatecla-definiciones/src/main/java/com/grupo5/definiciones/ChapterController.java
+++ santatecla-definiciones/src/main/java/com/grupo5/definiciones/ChapterController.java
@@ -4,8 +4,8 @@ import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,19 +31,15 @@ public class ChapterController {
 	@RequestMapping("/")
 	public String getChaptersAndConcepts(Model model, HttpServletRequest req, Pageable page) {
 		//Needed so it fetchs the concepts to avoid lazy initialization issues
-		Page<Chapter> chapters = chapterRepository.findAll(page);
-		
-		//model.addAttribute("anuncios", chapterRepository.findAll( page));
+		int elemsPerPage = 1;
+		Page<Chapter> chapters = chapterRepository.findAll(PageRequest.of(0, elemsPerPage, Sort.by(Direction.ASC, "chapterName")));
 		
 		model.addAttribute("empty", chapters.isEmpty());
 		model.addAttribute("chapters", chapters);
 		model.addAttribute("docente", req.isUserInRole("ROLE_DOCENTE"));
 		
-		model.addAttribute("showNext", !chapters.isLast());
-		model.addAttribute("showPrev", !chapters.isFirst());
-		model.addAttribute("numPage", chapters.getNumber());
-		model.addAttribute("nextPage", chapters.getNumber()+1);
-		model.addAttribute("prevPage", chapters.getNumber()-1);
+		model.addAttribute("showMore", !chapters.isLast());
+		model.addAttribute("nextPage", elemsPerPage);
 		
 		return "home";
 	}
