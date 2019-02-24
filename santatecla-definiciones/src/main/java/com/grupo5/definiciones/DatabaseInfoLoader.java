d39122c5397bc43d056b7c43420ac1dcec5ac752
ivchicano <ivchicano@gmail.com>
2019-02-24 02:19:11 +0100

    Add Question Entity

--- santatecla-definiciones/src/main/java/com/grupo5/definiciones/DatabaseInfoLoader.java
+++ santatecla-definiciones/src/main/java/com/grupo5/definiciones/DatabaseInfoLoader.java
@@ -9,9 +9,11 @@ import com.grupo5.definiciones.model.Answer;
 import com.grupo5.definiciones.model.Chapter;
 import com.grupo5.definiciones.model.Concept;
 import com.grupo5.definiciones.model.Justification;
+import com.grupo5.definiciones.model.Question;
 import com.grupo5.definiciones.model.User;
 import com.grupo5.definiciones.repositories.AnswerRepository;
 import com.grupo5.definiciones.repositories.ChapterRepository;
+import com.grupo5.definiciones.repositories.QuestionRepository;
 import com.grupo5.definiciones.repositories.UserRepository;
 
 @Component
@@ -25,6 +27,9 @@ public class DatabaseInfoLoader {
 
 	@Autowired
 	private AnswerRepository answerRepository;
+	
+	@Autowired
+	private QuestionRepository questionRepository;
 	
 	@PostConstruct
 	private void initDatabase() {
@@ -48,31 +53,36 @@ public class DatabaseInfoLoader {
 		chapterRepository.save(chapter1);
 		chapterRepository.save(chapter2);
 		chapterRepository.save(chapter3);
-
-		Answer[] answers = new Answer[4];
-		answers[0] = new Answer("¿ES CORRECTO AFIRMAR QUE " + "SPRING ES UN FRAMEWORK QUE PERMITE A "
-				+ "UNA APLICACIÓN EJECUTARSE TANTO EN UN " + "SERVIDOR DE APLICACIONES COMO EN UN " + "SERVIDOR WEB?",
-				"Sí", true, student);
-		answers[0].setCorrect(true);
-		answers[1] = new Answer("¿ES CIERTO QUE SPRING NO ES" + " UN FRAMEWORK QUE PERMITE EL DESARROLLO DE "
-				+ "APLICACIONES DE SERVIDOR PORQUE SUS " + "APLICACIONES SÓLO SE PUEDEN EJECUTAR EN UN "
-				+ "SERVIDOR WEB?", "Sí", true, student);
-		answers[1].setCorrect(false);
-		Justification j1 = new Justification("SUS APLICACIONES " + "SÓLO SE PUEDEN EJECUTAR EN UN SERVIDOR WEB?",
-				false);
-		j1.setError("SPRING PERMITE EL DESARROLLO DE DIVERSOS " + "TIPOS DE APLICACIONES DE SERVIDOR: APLICACIONES"
-				+ " WEB, SERVICIOS REST, ANÁLISIS DE DATOS BIG DATA...");
-		answers[1].setJustification(j1);
-		answers[2] = new Answer("¿QUÉ ES SPRING?",
-				"Un framework de " + "desarrollo de aplicaciones empresariales basado en" + " tecnologías Java.", false,
-				student);
-		answers[3] = new Answer("¿POR QUÉ NO ES CORRECTO AFIRMAR QUE " + "SPRING ES UN FRAMEWORK COMERCIAL?",
-				"Spring es un " + "framework de software libre.", false, admin);
 		
-		for (Answer a: answers) {
+		Answer[] answers = new Answer[4];
+		answers[0] = new Answer("UN FRAMEWORK QUE PERMITE A UNA APLICACIÓN EJECUTARSE TANTO EN UN SERVIDOR DE APLICACIONES COMO EN UN SERVIDOR WEB", true, student);
+		answers[0].setCorrect(true);
+		answers[1] = new Answer("UN FRAMEWORK DE DESARROLLO DE APLICACIONES WEB", true, student);
+		answers[1].setCorrect(false);
+		Justification j1 = new Justification("SÓLO SE PUEDE UTILIZAR PARA DESARROLLAR SERVICIOS REST",
+				false);
+		j1.setError("SPRING PERMITE EL DESARROLLO DE DIVERSOS TIPOS DE APLICACIONES DE SERVIDOR: APLICACIONES WEB, SERVICIOS REST, ANÁLISIS DE DATOS BIG DATA...");
+		answers[1].addJustification(j1);
+		answers[2] = new Answer("UN FRAMEWORK DE DESARROLLO DE APLICACIONES EMPRESARIALES BASADO EN JAVA", false,
+				student);
+		answers[3] = new Answer("UN FRAMEWORK COMERCIAL", true, admin);
+		answers[3].setCorrect(false);
+		Justification j2 = new Justification("ES UN FRAMEWORK DE SOFTWARE LIBRE", true);
+		answers[3].addJustification(j2);
+		for (Answer a : answers) {
 			a.setConcept(c11);
 			answerRepository.save(a);
 		}
+		
+		Question[] questions = new Question[4];
+		questions[0] = new Question("¿ES CORRECTO AFIRMAR QUE SPRING ES UN FRAMEWORK QUE PERMITE A UNA APLICACIÓN EJECUTARSE TANTO EN UN SERVIDOR DE APLICACIONES COMO EN UN SERVIDOR WEB?", answers[0]);
+		questions[1] = new Question("¿ES CIERTO QUE SPRING NO ES UN FRAMEWORK DE DESARROLLO DE APLICACIONES WEB PORQUE SÓLO SE PUEDE UTILIZAR PARA DESARROLLAR SERVICIOS REST?", answers[1]);
+		questions[2] = new Question("¿QUÉ ES SPRING?", answers[2]);
+		questions[3] = new Question("¿POR QUÉ NO ES CORRECTO AFIRMAR QUE SPRING ES UN FRAMEWORK COMERCIAL?", answers[3]);
+		for (Question q : questions) {
+			questionRepository.save(q);
+		}
+		
 	}
 
 }
