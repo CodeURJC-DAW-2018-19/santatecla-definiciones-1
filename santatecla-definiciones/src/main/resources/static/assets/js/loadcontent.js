a5d44869944f149acd4079ab037b0a7b1abc38d4
ivchicano <ivchicano@gmail.com>
2019-02-25 09:17:05 +0100

    Revert "Revert "Revert "Merge branch 'master' into question-generator"""
    This reverts commit 5c7902f0d83080bf4b59886b6c53341c4bca763d.

--- santatecla-definiciones/src/main/resources/static/assets/js/loadcontent.js
+++ santatecla-definiciones/src/main/resources/static/assets/js/loadcontent.js
@@ -1,98 +1,35 @@
-var chapters = 0;
-var conceptMap = new Map();
-var executedMap = new Map();
-var executedChapter = true;
+var map = new Map();
+map.set("chapters", 0);
+map.set("answers", 0);
 
-function loadGif(contentType){
-	$("#loadGif"+contentType).html('<img src="assets/gifs/ajax-loader.gif" />');
-}
-
-function unloadGif(contentType){
-	$("#loadGif"+contentType).html('');
-}
-function ajax(urlPage, contentType){
-	$.ajax({
+function loadChapters(){
+    $("#loadGif").html('<img src="assets/gifs/ajax-loader.gif" />');
+    var page = map.get("chapters");
+    var urlPage = "/loadChapters?page=" + map.get("chapters");
+    $.ajax({
         url: urlPage
     }).done(function(data){
-    	$("#"+contentType).append(data);
+        $("#accordion").append(data);
+        $("#loadGif").html('');
+        page++;
+        map.set("chapters", page);
     })
 }
 
 
-function newId(chapterId){
-	if(!conceptMap.has(chapterId)){
-		conceptMap.set(chapterId,0);
-		executedMap.set(chapterId,true);
-	}
-}
 
-
-function loadChapters(){
-	$(document).ready(function (){
-	    loadGif("Chapters"); 
-	    var urlPage = "/loadChapters?page=" + chapters;
-	    ajax(urlPage, "chapters");
-	    unloadGif("Chapters");
-	    chapters++;
-	});
-}
-
-function loadConcepts(chapterId){    
-    $(document).ready(function (){
-    	loadGif("Concept"+chapterId)
-    	newId(chapterId);
-    	var page = conceptMap.get(chapterId);
-        var urlPage = "/loadConcepts?chapterId="+chapterId+"&page="+ page;
-        ajax(urlPage, "concepts"+chapterId);
-        unloadGif("Concept"+chapterId);
-        page++;
-        conceptMap.set(chapterId, page);
-	});
-}
-
-
-
-function loadAnswersMarked(concept){
+function loadAnswers(concept){
 	//for some unknow reason we it gives an error in the console with GET gif
-    //$("#loadGifMarked").html('<img src="assets/gifs/ajax-loader.gif" />');
-    var page = map.get("answersMarked");
-    var urlPage = "/concept/"+concept+"?page=" + page + "&page="+ map.get("answersUnmarked");
+    //$("#loadGif").html('<img src="assets/gifs/ajax-loader.gif" />');
+    var page = map.get("answers");
+    var urlPage = "/concept/"+concept+"?page=" + page;
     $.ajax({
         url: urlPage
     }).done(function(data){
-        $("#Marked").append(data);
-        //$("#loadGifMarkedMarked").html('');
+    	console.log('hey');
+        $("#card-body").append(data);
+        //$("#loadGifMarked").html('');
         page++;
         map.set("answersMarked", page);
     })
 }
-
-
-
-function loadAnswersUnmarked(concept){
-	//for some unknow reason we it gives an error in the console with GET gif
-    //$("#loadGifUnmarked").html('<img src="assets/gifs/ajax-loader.gif" />');
-    var page = map.get("answersUnmarked");
-    var urlPage = "/concept/"+concept+"?page="+map.get("answersMarked")+"&page=" + page;
-    $.ajax({
-        url: urlPage
-    }).done(function(data){
-        $("#Unmarked").append(data);
-        //$("#loadGifMarkedUnmarked").html('');
-        page++;
-        map.set("answersUnmarked", page);
-    })
-}
-function triggerOnceChapter(){
-	if(executedChapter){
-		executedChapter = false;
-		alert('No hay más temas disponibles');
-	}
-}
-
-function triggerOnceConcept(id){
-	if(executedMap.get(id)){
-		executedMap.set(id, false);
-		alert('No hay más conceptos disponibles');
-	}
-};
