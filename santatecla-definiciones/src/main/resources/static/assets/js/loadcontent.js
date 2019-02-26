58c74aaf86ab7fb0e4e17d1547be8ae24e051d5d
LordHansTopo <32304288+LordHansTopo@users.noreply.github.com>
2019-02-26 15:25:16 +0100

    Refactorized loadcontent functions
    Refactorized and distributed loadcontent functions into several files

--- santatecla-definiciones/src/main/resources/static/assets/js/loadcontent.js
+++ santatecla-definiciones/src/main/resources/static/assets/js/loadcontent.js
@@ -1,10 +1,3 @@
-var chapters = 0;
-var executedChapter = true;
-
-var conceptMap = new Map();
-var executedMap = new Map(); //used for triggerOnceConcept function
-
-//General functions
 function loadGif(contentType){
 	$("#loadGif"+contentType).html('<img src="assets/gifs/ajax-loader.gif" />');
 }
@@ -19,60 +12,3 @@ function ajax(urlPage, contentType){
     	$("#"+contentType).append(data);
     })
 }
-
-function newId(chapterId){
-	if(!conceptMap.has(chapterId)){
-		conceptMap.set(chapterId,0);
-		executedMap.set(chapterId,true);
-	}
-}
-
-//Specific functions
-
-function loadChapters(){
-    $("#loadGif").html('<img src="assets/gifs/ajax-loader.gif" />');
-
-    var urlPage = "/loadChapters?page=" + chapters;
-    $.ajax({
-        url: urlPage
-    }).done(function(data){
-        $("#accordion").append(data);
-        $("#loadGif").html('');
-        chapters++;
-    })
-}
-
-function loadConcepts(chapterId){    
-    $(document).ready(function (){
-    	loadGif("Concept"+chapterId)
-    	newId(chapterId);
-    	var page = conceptMap.get(chapterId);
-        var urlPage = "/loadConcepts?chapterId="+chapterId+"&page="+ page;
-        ajax(urlPage, "concepts"+chapterId);
-        unloadGif("Concept"+chapterId);
-        page++;
-        conceptMap.set(chapterId, page);
-	});
-}
-
-
-/*function loadAnswers(concept){
-	//for some unknow reason we it gives an error in the console with GET gif
-    //$("#loadGif").html('<img src="assets/gifs/ajax-loader.gif" />');
-    var page = map.get("answers");
-    var urlPage = "/concept/"+concept+"?page=" + page;
-    $.ajax({
-        url: urlPage
-    }).done(function(data){
-    	console.log('hey');
-        $("#card-body").append(data);
-        //$("#loadGifMarked").html('');
-        page++;
-        map.set("answersMarked", page);
-    })
-}*/ //not working in theory
-
-
-//Trigger functions
-
-//TODO: implement/take from branch pagination5
