1d8fdd940689e3a97fe50426786e8d09a6669a43
ivchicano <ivchicano@gmail.com>
2019-03-04 19:10:56 +0100

    Receive random generated question

---
+++ santatecla-definiciones/src/main/java/com/group5/definitions/restcontrollers/RestConceptControllerStudent.java
@@ -0,0 +1,25 @@
+package com.group5.definitions.restcontrollers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.group5.definitions.model.Question;
+import com.group5.definitions.utilities.QuestionGenerator;
+
+@RestController
+@RequestMapping("/api")
+public class RestConceptControllerStudent {
+	
+	@Autowired
+	private QuestionGenerator questionGenerator;
+	
+	@JsonView(Question.Basic.class)
+	@GetMapping("/concept/{id}/questionGenerate")
+	public Question generateQuestion(@PathVariable long id) {
+		return questionGenerator.generateQuestion(id);
+	}
+}
