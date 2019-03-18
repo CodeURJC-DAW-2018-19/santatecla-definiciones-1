db7a6422ee6e09f2749fd07a9d668412125e08bc
ivchicano <ivchicano@gmail.com>
2019-03-19 00:05:49 +0100

    Add more tab functionality

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,15 +6,19 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
+
 import { CovalentLayoutModule } from '@covalent/core/layout';
 import { CovalentStepsModule  } from '@covalent/core/steps';
 import { CovalentTabSelectModule } from '@covalent/core/tab-select';
 
 import { HeaderComponent } from "./header.component";
+import { TestComponent } from "./test.component";
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
--- angular/src/app/header.component.ts
+++ angular/src/app/header.component.ts
@@ -1,8 +1,24 @@
-import {Component} from '@angular/core'
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
+interface Tab {
+  name: string;
+  route: string;
+}
 @Component({
-    selector: 'header',
-    templateUrl: './header.component.html'
+  selector: "header",
+  templateUrl: "./header.component.html"
 })
 export class HeaderComponent {
+  tabs: Tab[] = [{ name: "Inicio", route: "" }];
+
+  constructor(private router: Router) {}
+
+  addTab(name: string, route: string) {
+    this.tabs.push({ name: name, route: route });
+  }
+
+  routeTab(route:string) {
+    this.router.navigate([route]);
+  }
 }
