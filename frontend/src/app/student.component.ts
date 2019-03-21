a953a7a7440ababc4ee9ecd9a83d6a09522fc1fd
Diego <diegojaraoro@gmail.com>
2019-03-21 15:18:51 +0100

    Fixed student CSS

--- frontend/src/app/student.component.ts
+++ frontend/src/app/student.component.ts
@@ -8,7 +8,8 @@ import { QuestionPage } from './questionPage.model';
 
 @Component({
     selector: "student",
-    templateUrl: "./student.component.html"
+    templateUrl: "./student.component.html",
+    styleUrls: ["./student.component.css"]
   })
 
 export class StudentComponent {
