da98238502bdc16b5a28a04ff831e50eeadaa9b5
ivchicano <ivchicano@gmail.com>
2019-03-26 10:31:26 +0100

    Fix login and logout

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { MatTableModule } from "@angular/material/table";
 
@@ -40,6 +40,9 @@ import { DiagramService } from "./diagram/diagram.service";
 import { QuestionsService } from "./student/question.service";
 import { AnswerService } from "./teacher/answer.service";
 import { TeacherService } from "./teacher/teacher.service";
+import { BasicAuthInterceptor } from './login/auth.interceptor';
+import { ErrorInterceptor } from './login/error.interceptor';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -75,7 +78,10 @@ import { TeacherService } from "./teacher/teacher.service";
     MatPaginatorModule,
     CovalentExpansionPanelModule
   ],
-  providers: [ChapterService, QuestionsService, TeacherService, AnswerService, DiagramService, LoginService],
+  providers: [ChapterService, QuestionsService, TeacherService, AnswerService, DiagramService, LoginService,
+    { provide: HTTP_INTERCEPTORS, useClass: BasicAuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent, DiagramComponent]
 })
 export class AppModule {}
--- frontend/src/app/login/login.service.ts
+++ frontend/src/app/login/login.service.ts
@@ -1,101 +1,76 @@
-import { Injectable } from "@angular/core";
-import {
-  HttpClient,
-  HttpHeaders,
-  HttpErrorResponse
-} from "@angular/common/http";
-import { map } from "rxjs/operators";
-import { environment } from "../../environments/environment";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment'
+
+const URL = environment.baseUrl;
 
 export interface User {
-  id?: number;
-  name: string;
-  roles: string[];
+    id?: number;
+    name: string;
+    roles: string[];
+    authdata: string;
 }
-const apiUrl = "/api";
+
 @Injectable()
 export class LoginService {
-  isLogged = false;
-  isAdmin = false;
-  user: User;
-  auth: string;
 
-  constructor(private http: HttpClient) {
-    this.reqIsLogged();
-  }
+    isLogged = false;
+    isAdmin = false;
+    user: User;
+    auth: string;
 
-  getAuth() {
-    return this.auth;
-  }
-
-  reqIsLogged(): void {
-    const headers = new HttpHeaders({
-      "X-Requested-With": "XMLHttpRequest",
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Origin": "*",
-      "Cache-Control": "no-cache" // due to IE browser caches API get requests
-    });
-
-    this.http
-      .get(apiUrl + "/logIn", { withCredentials: true, headers })
-      .subscribe(
-        (response: User) => this.processLogInResponse(response),
-        (error: HttpErrorResponse) => {
-          if (error.status !== 401) {
-            console.error(
-              "Error when asking if logged: " + JSON.stringify(error)
-            );
-          }
+    constructor(private http: HttpClient) {
+        //TODO: Delete clear() when logout works
+        localStorage.clear();
+        let user = JSON.parse(localStorage.getItem('currentUser'));
+        if (user) {
+            console.log('Logged user');
+            this.setCurrentUser(user);
         }
-      );
-  }
+    }
 
-  private processLogInResponse(response: User) {
-    this.isLogged = true;
-    this.user = response;
-    this.isAdmin = this.user.roles.indexOf("ROLE_ADMIN") !== -1;
-  }
+    logIn(user: string, pass: string) {
 
-  logIn(user: string, pass: string) {
-    console.log("user:" + user + " pass:" + pass);
+        let auth = window.btoa(user + ':' + pass);
 
-    this.auth = "Basic " + utf8_to_b64(user + ":" + pass);
+        const headers = new HttpHeaders({
+            Authorization: 'Basic ' + auth,
+            'X-Requested-With': 'XMLHttpRequest',
+        });
 
-    const headers = new HttpHeaders({
-      Authorization: this.auth,
-      "X-Requested-With": "XMLHttpRequest",
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Origin": "*",
-      "Cache-Control": "no-cache" // due to IE browser caches API get requests
-    });
+        return this.http.get<User>('/api/logIn', { headers })
+            .pipe(map(user => {
 
-    return this.http
-      .get(apiUrl + "/logIn", { withCredentials: true, headers })
-      .pipe(
-        map((response: User) => {
-          this.processLogInResponse(response);
-          return this.user;
-        })
-      );
-  }
+                if (user) {
+                    this.setCurrentUser(user);
+                    user.authdata = auth;
+                    localStorage.setItem('currentUser', JSON.stringify(user));
+                }
 
-  logOut() {
-    return this.http.get(apiUrl + "/logOut", { withCredentials: true }).pipe(
-      map(response => {
+                return user;
+            }));
+    }
+
+    logOut() {
+
+        return this.http.get(URL + '/logOut').pipe(
+            map(response => {
+                this.removeCurrentUser();
+                return response;
+            }),
+        );
+    }
+
+    private setCurrentUser(user: User) {
+        this.isLogged = true;
+        this.user = user;
+        this.isAdmin = this.user.roles.indexOf('ROLE_ADMIN') !== -1;
+    }
+
+    removeCurrentUser() {
+        localStorage.removeItem('currentUser');
         this.isLogged = false;
         this.isAdmin = false;
-        return response;
-      })
-    );
-  }
-}
-
-function utf8_to_b64(str) {
-  return btoa(
-    encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
-      return String.fromCharCode(<any>"0x" + p1);
-    })
-  );
+    }
 }
