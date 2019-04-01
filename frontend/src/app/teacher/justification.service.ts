60fb6ebafc4f7a3c04d875f5c272fa4c0a83ab47
David <32681308+ggeettaa@users.noreply.github.com>
2019-04-01 18:58:53 +0200

    done addjust

--- frontend/src/app/teacher/justification.service.ts
+++ frontend/src/app/teacher/justification.service.ts
@@ -30,9 +30,18 @@ export class JustificationService {
                }
            }));
    }
+   
 
    private handleError(error: any) {
       console.error(error);
       return throwError(new Error('Server error (' + error.status + '): ' + error));
    }
+   
+   postNewJustification(id: number, just: Justification){
+    return this.http.post(
+        this.apiUrl   + "/answers/" + id + "/justifications", 
+        just,
+        { withCredentials: true }
+    );
+  }
 }
